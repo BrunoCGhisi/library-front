030cf8d12793a36faddd783b75f3c3136d88070a
BrunoCGhisi <brunocostaghisi@gmail.com>
2024-06-06 17:23:59 -0300

    inicio drawer

---
+++ src/components/drawer.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react' ;
+
+
+const admDrawer = () => {
+    const [open, setOpen] = React.useState(false);
+  
+    const  = (newOpen: boolean) => () => {
+      setOpen(newOpen);
+    };
+
+const admDrawer = {
+    
+}
+}
+export admDrawer
